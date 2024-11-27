d99d2468876db3649f03f1662594b11f39172ae2
Pablo <pablosgar96@gmail.com>
2024-11-27 14:10:54 +0100

    refactor-v0.5

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Shito-Ryu Club | Inicio</title>
-    <link rel="stylesheet" href="./css/style.css">
-    <script defer src="./js/app_socios.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <script defer src="js/app.js"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Manrope:wght@200..800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
@@ -20,11 +20,11 @@
             // include './php/cabecera.php';
             //getcwd() devuelve el directorio actual
             $ruta_i="#";
-            $ruta_soc="./paginas/socios.php";
-            $ruta_serv="./paginas/servicios.php";
-            $ruta_tes="./paginas/testimonios.php";
+            $ruta_soc="./paginas/socios/socios.php";
+            $ruta_serv="./paginas/servicios/servicios.php";
+            $ruta_tes="./paginas/testimonios/testimonios.php";
             $ruta_not="./paginas/noticias/noticias.php";
-            $ruta_cit="./paginas/citas.php";
+            $ruta_cit="./paginas/citas/citas.php";
             echo dibujarCabecera($ruta_i, $ruta_soc, $ruta_serv, $ruta_tes, $ruta_not, $ruta_cit);
         ?>
 
--- paginas/testimonios/testimonios.php
+++ paginas/testimonios/testimonios.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Shito-Ryu Club | Testimonios</title>
-    <link rel="stylesheet" href="../css/style.css">
-    <link rel="stylesheet" href="../css/style_testimonios.css">
-    <script defer src="../js/app_socios.js"></script>
+    <link rel="stylesheet" href="../../css/style.css">
+    <link rel="stylesheet" href="../../css/style_testimonios.css">
+    <script defer src="../../js/app.js"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Manrope:wght@200..800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
@@ -14,15 +14,15 @@
 <body>
     <main class='principal-testimonios'>
         <?php
-            require_once '../php/funciones.php';
-            require_once '../php/config.php';
+            require_once '../../php/funciones.php';
+            require_once '../../php/config.php';
             $conexion=conectar($nombre_host, $nombre_usuario, $password_db, $nombre_db);
-            $ruta_i="../index.php";
-            $ruta_soc="socios.php";
-            $ruta_serv="servicios.php";
+            $ruta_i="../../index.php";
+            $ruta_soc="../socios/socios.php";
+            $ruta_serv="../servicios/servicios.php";
             $ruta_tes="#";
-            $ruta_not="./noticias/noticias.php";
-            $ruta_cit="citas.php";
+            $ruta_not="../noticias/noticias.php";
+            $ruta_cit="../citas/citas.php";
             echo dibujarCabecera($ruta_i, $ruta_soc, $ruta_serv, $ruta_tes, $ruta_not, $ruta_cit);
         ?>
 
@@ -56,7 +56,7 @@
     </main>
 
     <?php 
-        include '../php/footer.php';
+        include '../../php/footer.php';
         $conexion->close();
     ?>
 </body>
